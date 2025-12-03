71202a7f6f363146b81896e50b66e24c5b3e3f52
dion-choy <dionchoynj@gmail.com>
2025-12-03 23:16:46 +0800

    day3 finished

--- day3.py
+++ day3.py
@@ -1,18 +1,6 @@
 with open("day3.txt") as txt:
     input = txt.read().strip().split("\n")
 
-# txt = '''987654321111111
-# 811111111111119
-# 234234234234278
-# 818181911112111'''
-
-txt = '''2232212212212222211221231124224222213132222133122224222123222112324122222122221322222225222342243112
-2222312114222314322223142323251122424322142244122212213222222222242526225232262212234353242222112242
-3325444446453345434534234244443254434533443354443344424431124344542134454433534353443542455544444483
-2334235313232232645234524223233333333323323333233334323332325633143222333322242333132233233132212325
-2234311224123424222222224432222333212224412234223312244413252153222112433222334322772215422121224273'''
-input = txt.split("\n")
-
 def part1():
     count = 0
     for i in input:
@@ -29,46 +17,23 @@ def part2():
     count = 0
     for i in input:
         num = ""
-        first_digit = sorted(i[:-12])[-1]
-        num += first_digit
 
-        remaining = i[i.index(first_digit):]
-        # while(len(remaining)>12):
-        next_highest = sorted(remaining[1:])
+        index = [-1]
+        for digit in range(12, 0, -1):
+            maxPtr = len(i)-digit
+            for curPtr in range(len(i)-digit, index[-1], -1):
+                if int(i[curPtr]) >= int(i[maxPtr]):
+                    maxPtr = curPtr
+            index.append(maxPtr)
 
-        while len(remaining) - remaining.index(next_highest[-1], 1) >= 12:
-            num += next_highest[-1]
-            print("NUM", num)
-            remaining = remaining[remaining.index(next_highest[-1], 1):]
-            next_highest = sorted(remaining[1:])
+        num = ""
+        index.pop(0)
+        for j in index:
+            num += i[j]
 
-
-        print(i)
-        print(remaining)
-        print(next_highest)
-        print(next_highest[-1])
-        print(remaining.index(next_highest[-1], 1))
-
-        if len(remaining) - remaining.index(next_highest[-1], 1) < 12:
-            print("NOT ENOUGH LENGTH", len(remaining) - remaining.index(next_highest[-1], 1))
-            print("NUM", num)
-
-        # if len()
-        # num_str = first_digit + remaining[remaining.index(next_highest[-2], 1)]
-        #
-        # print(num_str)
-
-        remaining = i[i.index(first_digit):]
-
-        # count += int(remaining)
+        count += int(num)
 
     return count
-
-# 2222312114222314322223142323251122424322142244122212213222222222242526225232262212234353242222112242
-# 6225232262212234353242222112242
-# 665422222242
-# 653634353442
-# 656454222242
 
 print(part1())
 print(part2())
