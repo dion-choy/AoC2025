21e03449d4e8be6e28463e7e1e59793d8c1af53a
dion-choy <dionchoynj@gmail.com>
2025-12-07 01:06:53 +0800

    day 6 AoC 2025 delayed by checking out unis

---
+++ day6.py
@@ -0,0 +1,46 @@
+with open("./day6.txt") as txt:
+    input = txt.read().split("\n")
+
+def part1():
+    ls = [[k for k in j if k != ""] for j in [i.split(" ") for i in input if i != ""]]
+
+    count = 0
+    for j in range(len(ls[0])):
+        cur = 1
+        for i in range(len(ls)-1):
+            if ls[-1][j] == "+":
+                cur += int(ls[i][j])
+            else:
+                cur *= int(ls[i][j])
+
+        count += cur-1 if ls[-1][j] == "+" else cur
+
+    return count
+
+def part2():
+    ls = [i for i in input if i != ""]
+
+    count = 0
+    col = 0
+    cur = 1
+    for j in range(len(ls[0])):
+        num = ""
+        for i in range(len(ls)-1):
+            num += ls[i][j] if ls[i][j] != " " else ""
+
+        if num.strip() == "":
+            count += cur-1 if [i for i in ls[-1].split(" ") if i != ""][col] == "+" else cur
+            cur = 1
+            col += 1
+        else:
+            if [i for i in ls[-1].split(" ") if i != ""][col] == "+":
+                cur += int(num)
+            else:
+                cur *= int(num)
+
+    count += cur-1 if [i for i in ls[-1].split(" ") if i != ""][col] == "+" else cur
+
+    return count
+
+print(part1())
+print(part2())
