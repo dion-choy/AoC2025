c559928c396373ef7759469fdaa03982d9994969
dion-choy <dionchoynj@gmail.com>
2026-01-11 12:26:41 +0800

    change style for let ... in

--- day2.ml
+++ day2.ml
@@ -2,7 +2,6 @@
 let input = List.map (fun [a; b] -> (a, b)) (
     List.map (String.split_on_char '-') (
         String.split_on_char ',' (String.trim (In_channel.with_open_text "./day2.txt" In_channel.input_all))
-        (* String.split_on_char ',' (String.trim (In_channel.with_open_text "./test.txt" In_channel.input_all)) *)
         )
     )
 [@@@warning "+8"]
@@ -10,41 +9,44 @@ let input = List.map (fun [a; b] -> (a, b)) (
 let part1 input = 
     let check_num num_str =
         if String.length num_str mod 2 = 0 then
-            if (String.sub num_str 0 (String.length num_str / 2)) = (String.sub num_str (String.length num_str / 2) (String.length num_str / 2))
+            if (String.sub num_str 0 (String.length num_str / 2)) =
+                (String.sub num_str (String.length num_str / 2) (String.length num_str / 2))
             then int_of_string num_str
             else 0
-        else 0
-    in let rec range num target =
-        if int_of_string num > int_of_string target then 0 else check_num num + range (string_of_int (int_of_string num +1)) target
-    in let rec loop lst =
+        else 0 in
+    let rec range num target =
+        if int_of_string num > int_of_string target then 0
+        else check_num num + range (string_of_int (int_of_string num +1)) target in
+    let rec loop lst =
         match lst with
         | head :: tail -> range (fst head) (snd head) + loop tail
-        | [] -> 0
-    in loop input
+        | [] -> 0 in
+    loop input
 
 let part2 input = 
     let rec all_equal prev lst =
         match lst with
         | first :: tail -> (first = prev) && (all_equal first tail)
-        | [] -> true
-    in let rec split_str num str =
+        | [] -> true in
+    let rec split_str num str =
         if String.length str = 0 then []
-        else String.sub str 0 num :: split_str num (String.sub str num (String.length str - num))
-    in let rec repeats num num_str =
+        else String.sub str 0 num :: split_str num (String.sub str num (String.length str - num)) in
+    let rec repeats num num_str =
         if num = String.length num_str then 0
         else 
             if String.length num_str mod num = 0 then 
                 if all_equal (String.sub num_str 0 num) (split_str num num_str) then
                     int_of_string num_str
                 else repeats (num+1) num_str
-            else repeats (num+1) num_str
-    in let rec range num target =
-        if int_of_string num > int_of_string target then 0 else repeats 1 num + range (string_of_int (int_of_string num +1)) target
-    in let rec loop lst =
+            else repeats (num+1) num_str in
+    let rec range num target =
+        if int_of_string num > int_of_string target then 0
+        else repeats 1 num + range (string_of_int (int_of_string num +1)) target in
+    let rec loop lst =
         match lst with
         | head :: tail -> range (fst head) (snd head) + loop tail
-        | [] -> 0
-    in loop input
+        | [] -> 0 in
+    loop input
 
 let time f x =
     let t = Sys.time() in
--- day3.ml
+++ day3.ml
@@ -1,39 +1,39 @@
 let input =
-    let explode str = List.init (String.length str) (String.get str)
-    in List.map (fun x -> List.map (fun x -> int_of_char x - int_of_char '0') (explode x)) (
-        String.split_on_char '\n' (String.trim (In_channel.with_open_text "./day3.txt" In_channel.input_all))
-        )
+    let explode str = List.init (String.length str) (String.get str) in
+    List.map (
+        fun x -> List.map (fun x -> int_of_char x - int_of_char '0') (explode x))
+    (String.split_on_char '\n' (String.trim (In_channel.with_open_text "./day3.txt" In_channel.input_all)))
 
 let part1 input =
-    let find_max lst = List.fold_left max 0 lst
-    in let find_index_max lst = 
-        Option.get (List.find_index (fun x -> x = find_max lst) lst)
-    in let process lst =
+    let find_max lst = List.fold_left max 0 lst in
+    let find_index_max lst = 
+        Option.get (List.find_index (fun x -> x = find_max lst) lst) in
+    let process lst =
         find_max (List.take (List.length lst -1) lst) * 10
-        + find_max (List.drop (find_index_max (List.take (List.length lst -1) lst) +1) lst)
-    in let rec loop lst =
+        + find_max (List.drop (find_index_max (List.take (List.length lst -1) lst) +1) lst) in
+    let rec loop lst =
         match lst with
         | first :: tail -> loop tail + process first
-        | [] -> 0
-    in loop input
+        | [] -> 0 in
+    loop input
 
 let part2 input =
-    let rec exp num ex = if ex > 0 then exp num (ex-1) * num else 1
-    in let find_max lst = List.fold_left max 0 lst
-    in let find_index_max lst = 
-        Option.get (List.find_index (fun x -> x = find_max lst) lst)
-    in let rec process cnt row =
-        let sub_row = List.take ((List.length row)-cnt) row
-        in if cnt >= 0 then
+    let rec exp num ex = if ex > 0 then exp num (ex-1) * num else 1 in
+    let find_max lst = List.fold_left max 0 lst in
+    let find_index_max lst = 
+        Option.get (List.find_index (fun x -> x = find_max lst) lst) in
+    let rec process cnt row =
+        let sub_row = List.take ((List.length row)-cnt) row in
+        if cnt >= 0 then
             process (cnt-1) (
                 List.drop (find_index_max sub_row +1) row
                 ) + (exp 10 cnt * find_max sub_row)
-        else 0
-    in let rec loop lst =
+        else 0 in
+    let rec loop lst =
         match lst with
         | first :: tail -> loop tail + process 11 first
-        | [] -> 0
-    in loop input
+        | [] -> 0 in
+    loop input
 
 let time f x =
     let t = Sys.time() in
